760cba1615beb1cea0e27c6df5a6da967127b003
Kzlyr Shaira Manejo <kmanejo@up.edu.ph>
2025-07-30 21:12:25 +0800

    feat: update login and sign up

--- src/components/main pages/login.js
+++ src/components/main pages/login.js
@@ -5,7 +5,22 @@ const Login = () =>{
 
     return(
         <Container>
-            This is a login page.
+            <Form>
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        Never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Label>Enter Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter password" />
+
+                <Button type="submit" className="primary-button mt-3">
+                    Submit
+                </Button>
+            </Form>
         </Container>
     )
 
--- src/components/main pages/signup.js
+++ src/components/main pages/signup.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Signup = () => {
 
     return (
         <Container>
-            This is a sign up page.
+            <Form>
+                <div className="d-flex gap-3">
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="name" placeholder="Enter first name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="name" placeholder="Enter last name" />
+                    </Form.Group>
+                </div>
+
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                
+                <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Re-enter password" />
+                </Form.Group>
+
+                <Button type="submit" className="primary-button mt-3">
+                    Submit
+                </Button>
+            </Form>
         </Container>
     )
 }
